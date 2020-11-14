00c8bb3b6cff409c829d8f79055e040b0c1ad835
Christiaan Kruger <c@hristiaan.co.za>
2020-11-14 12:36:50 +0200

    Accept port as parameter

--- src/traditional/index.ts
+++ src/traditional/index.ts
@@ -1,11 +1,21 @@
 import { makeApp } from "../app";
+import yargs from "yargs";
 
-export const initialize = async () => {
+export interface InitializeConfiguration {
+  port: number;
+}
+
+export const initialize = async (configuration: InitializeConfiguration) => {
   const app = await makeApp({ context: {} });
   app.use(async (ctx) => {
     ctx.body = "Hello, world!";
   });
-  app.listen(4175);
+  app.listen(configuration.port, () => {
+    console.log(`Traditional server running on port ${configuration.port}.`);
+  });
 };
 
-initialize();
+const argv = yargs(process.argv).argv;
+
+const port = (argv.port as number) || 4175;
+initialize({ port });
